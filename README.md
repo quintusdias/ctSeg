8f263a99cd8663b96d62ad895df4195703299369
John Evans <john.g.evans.ne@gmail.com>
2015-04-10 16:49:59 -0400

    Make into a package
    Allows use of entry points to define our executable.
    Don't keep re-instancing cursor and connection objects, re-use the ones
    instanced in the object constructor.  Set the initial version of the
    package to 0.0.3, assume that 0.0.1 was what was present when the
    project was left off in spring of 2014.  v0.0.2 is when we got it
    working again on hub zero.

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Requirements
 ------------
 * scikit-image 0.11.3
 * Pillow 2.7.8
+* networkx 1.9.1
+* decorator >= 3.4.0
 * Python2
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+kwargs = {'name': 'ctSeg',
+          'description': 'NCIPHUB Tool for MOIST run segmentation comparison',
+          'version': '0.0.3',
+          'long_description': open('README.md').read(),
+          'author': 'John Evans',
+          'author_email': 'john.g.evans.ne@gmail.com',
+          'url': 'https://github.com/quintusdias/ctSeg',
+          'packages': ['ctSeg'],
+          'package_data': {'ctSeg': ['share/*.db']},
+          'entry_points': {
+              'console_scripts': ['run_ctseg=ctSeg.command_line:run_ctseg',
+                                  'make_ctseg_db=ctSeg.command_line:make_db'],
+              },
+          'install_requires': ['scikit_image>=0.11.3', 'Pillow>=2.7.8'],
+          'license': 'MIT'}
+
+setup(**kwargs)
