63320d105a0f18b4f472d8a5a621579d3597e73c
John Evans <john.g.evans.ne@gmail.com>
2015-04-11 05:44:19 -0400

    Replace print statements with logger
    Right now the logger module just echos the arguments that used to be fed
    to the print function, but the logging could potentially be made more
    elaborate.
    SQL statements changed to use placeholders style.
    Some PEP8 cleanup.
    
    Closes:  #5

--- ctSeg/ctSeg.py
+++ ctSeg/ctSeg.py
@@ -1,4 +1,5 @@
 # -*- coding:  utf-8 -*-
+import logging
 import os
 import pkg_resources as pkg
 import re
@@ -16,6 +17,7 @@ import scipy.misc
 
 from .ctSeg_ui import Ui_Dialog
 
+
 class CtSegForm(QtGui.QDialog):
     """
     Attributes
@@ -24,6 +26,9 @@ class CtSegForm(QtGui.QDialog):
         Label identifying the base image currently active.  May be something
         like L0013.
     conn, cursor : database connection objects
+        Handles database queries
+    dataroot : str
+        Path to where the base images and pairwise segmentations are stored.
     """
     def __init__(self, parent=None):
         """
@@ -32,6 +37,8 @@ class CtSegForm(QtGui.QDialog):
             self.dataroot = '/data/groups/qinportal/ctSeg/resultsNii'
         else:
             self.dataroot = '/data/mgh/resultsNii'
+
+        logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_Dialog()
@@ -48,11 +55,9 @@ class CtSegForm(QtGui.QDialog):
                                self.execute)
 
         # When the slider is changed, load a new slice.
-        #self.ui.imageSliceSlider.valueChanged.connect(self.load_new_slice)
         QtCore.QObject.connect(self.ui.imageSliceSlider,
                                QtCore.SIGNAL('sliderReleased()'),
                                self.load_new_slice)
-
 
     def load_new_slice(self):
         """
@@ -60,7 +65,6 @@ class CtSegForm(QtGui.QDialog):
         """
         slice_number = self.ui.imageSliceSlider.sliderPosition()
         self.display_image_slice(slice_number)
-
 
     def setup_database(self):
         """
@@ -75,7 +79,7 @@ class CtSegForm(QtGui.QDialog):
         Setup paths to be able to run C3D.
         """
         # Add some common paths to the c3d executable
-        # First is on HubZero, 2nd is Mac with gui installation, 3rd is 
+        # First is on HubZero, 2nd is Mac with gui installation, 3rd is
         # MGH getafix.
         paths = ['/apps/share64/debian7/convert3d/nightly',
                  '/Applications/Convert3DGUI.app/Contents/bin',
@@ -104,25 +108,22 @@ class CtSegForm(QtGui.QDialog):
 
         command = 'c3d -verbose {image1} {image2} -overlap 1'
         command = command.format(image1=self.image_1, image2=self.image_2)
-        print(command)
+        logging.info(command)
         self.c3d_output = command
         self.c3d_output += '\n'
         output = subprocess.check_output(command.split(' '), env=self.env)
         matchobj = regex.search(output)
-        print(float(matchobj.group('dice')))
+        logging.info(output)
         self.ui.diceLabel.setText(matchobj.group())
         return float(matchobj.group('dice'))
-
 
     def execute(self):
         """
         Call C3D, get the dice coefficient.
         """
-        # What run images were chosen?
+        # What pairwise images were chosen?
         idx1 = self.ui.team1ComboBox.currentIndex()
         idx2 = self.ui.team2ComboBox.currentIndex()
-        print("idx1 = ", idx1)
-        print("idx2 = ", idx2)
 
         qvar = self.ui.team1ComboBox.itemData(idx1, role=QtCore.Qt.UserRole)
         challenge_id1 = int(qvar.toString())
@@ -131,31 +132,32 @@ class CtSegForm(QtGui.QDialog):
 
         sql = """
               SELECT file FROM challenge
-              WHERE id = ? 
+              WHERE id = :id
               """
-        self.cursor.execute(sql, (challenge_id1,))
-        print(sql, challenge_id1)
+        self.cursor.execute(sql, {'id': challenge_id1})
+        logging.info(sql)
+        logging.info(str(challenge_id1))
 
         row = self.cursor.fetchone()
         file1 = row[0]
 
-
         sql = """
               SELECT file FROM challenge
-              WHERE id = ?
+              WHERE id = :id
               """
-        self.cursor.execute(sql, (challenge_id2,))
-        print(sql, challenge_id2)
+        self.cursor.execute(sql, {'id': challenge_id2})
+        logging.info(sql)
+        logging.info(str(challenge_id2))
 
         row = self.cursor.fetchone()
         file2 = row[0]
-        print("file1 = ", file1)
-        print("file2 = ", file2)
+        logging.info("file1 = {}".format(file1))
+        logging.info("file2 = {}".format(file2))
         self.image_1 = os.path.join(self.dataroot, file1)
         self.image_2 = os.path.join(self.dataroot, file2)
-        print("loading {}".format(self.image_1))
+        logging.info("loading {}".format(self.image_1))
         self.image1_data = nib.load(self.image_1).get_data()
-        print("loading {}".format(self.image_2))
+        logging.info("loading {}".format(self.image_2))
         self.image2_data = nib.load(self.image_2).get_data()
         flt = self.run_c3d()
 
@@ -171,27 +173,29 @@ class CtSegForm(QtGui.QDialog):
         height, width, depth = self.image_data.shape
 
         # Retrieve user image contours at the specified slice.
-        slice_contours_1 = skimage.measure.find_contours(self.image1_data[:, :, slice_number], 0.8)
-        slice_contours_2 = skimage.measure.find_contours(self.image2_data[:, :, slice_number], 0.8)
+        slice1 = self.image1_data[:, :, slice_number]
+        slice_contours_1 = skimage.measure.find_contours(slice1, 0.8)
+        slice2 = self.image2_data[:, :, slice_number]
+        slice_contours_2 = skimage.measure.find_contours(slice2, 0.8)
 
-        image_slice = scipy.misc.bytescale(self.image_data[:,:,slice_number])
+        image_slice = scipy.misc.bytescale(self.image_data[:, :, slice_number])
         faux_3d = np.zeros((height, width, 3))
-        faux_3d[:,:,0] = image_slice
-        faux_3d[:,:,1] = image_slice
-        faux_3d[:,:,2] = image_slice
+        faux_3d[:, :, 0] = image_slice
+        faux_3d[:, :, 1] = image_slice
+        faux_3d[:, :, 2] = image_slice
 
         # Color user image 1 contours as red
         for contour in slice_contours_1:
-            rows = contour[:,0].round().astype(np.int32)
-            cols = contour[:,1].round().astype(np.int32)
+            rows = contour[:, 0].round().astype(np.int32)
+            cols = contour[:, 1].round().astype(np.int32)
             faux_3d[rows, cols, 0] = 0
             faux_3d[rows, cols, 1] = 0
             faux_3d[rows, cols, 2] = 255
 
         # Color user image 2 contours as green
         for contour in slice_contours_2:
-            rows = contour[:,0].round().astype(np.int32)
-            cols = contour[:,1].round().astype(np.int32)
+            rows = contour[:, 0].round().astype(np.int32)
+            cols = contour[:, 1].round().astype(np.int32)
             faux_3d[rows, cols, 0] = 0
             faux_3d[rows, cols, 1] = 255
             faux_3d[rows, cols, 2] = 0
@@ -204,11 +208,10 @@ class CtSegForm(QtGui.QDialog):
 
         pixmap = QtGui.QPixmap(qimage)
 
-        # QGraphicsView.fitInView()?
-        myScaledPixmap = pixmap.scaled(self.ui.graphicsView.size(), QtCore.Qt.KeepAspectRatio)
+        myScaledPixmap = pixmap.scaled(self.ui.graphicsView.size(),
+                                       QtCore.Qt.KeepAspectRatio)
         self.scene.addPixmap(myScaledPixmap)
         self.scene.update()
-
 
     def setupBaseImage(self):
         """
@@ -233,18 +236,19 @@ class CtSegForm(QtGui.QDialog):
         self.ui.graphicsView.setScene(self.scene)
 
         self.display_image_slice(0)
-        
 
     def setupCollectionTree(self):
         """
         Populate the collection/base image tree from the sqlite3 database.
         """
         self.ui.collectionTreeWidget.setColumnCount(2)
-        self.ui.collectionTreeWidget.setHeaderLabels(['Collection', 'Base Image'])
-        QtCore.QObject.connect(self.ui.collectionTreeWidget,
-                               QtCore.SIGNAL('itemClicked(QTreeWidgetItem*, int)'),
-                               self.treeItemClicked)
-
+        self.ui.collectionTreeWidget.setHeaderLabels(['Collection',
+                                                      'Base Image'])
+        QtCore.QObject.connect(
+            self.ui.collectionTreeWidget,
+            QtCore.SIGNAL('itemClicked(QTreeWidgetItem*, int)'),
+            self.treeItemClicked
+        )
 
         # setup the top level items.  These are just the collection names.
         sql = """
@@ -263,10 +267,10 @@ class CtSegForm(QtGui.QDialog):
                   FROM base_image
                   INNER JOIN collection
                       ON base_image.collection_id = collection.id
-                  WHERE collection.name = ?
+                  WHERE collection.name = :collection_name
                   """
-            print(sql)
-            self.cursor.execute(sql, (collection_name,))
+            logging.info(sql)
+            self.cursor.execute(sql, {'collection_name': collection_name})
             base_image_rows = self.cursor.fetchall()
             for base_image_id, base_image_relpath, label in base_image_rows:
                 leaf = QtGui.QTreeWidgetItem(branch)
@@ -285,19 +289,19 @@ class CtSegForm(QtGui.QDialog):
         self.ui.team2ComboBox.clear()
 
         label = leaf.data(column, QtCore.Qt.UserRole).toString()
-        print("Base image id = {0}".format(label))
+        logging.info("Base image id = {0}".format(label))
         self.label = label
-        print(type(label))
 
         # Get the runs associated with the base image and populate the combo
         # boxes.
         sql = """
               SELECT c.id, t.team, c.run_id
               FROM challenge c INNER JOIN team t on t.id = c.team_id
-              WHERE label = ?
+              WHERE label = :label
               """
-        self.cursor.execute(sql, (str(label),))
-        print(sql, label)
+        logging.info("sql:  {}".format(sql))
+        logging.info("label:  {}".format(label))
+        self.cursor.execute(sql, {'label': str(label)})
         rows = self.cursor.fetchall()
         for challenge_id, team, run_id in rows:
             name = "{0}-{1}".format(team, run_id)
