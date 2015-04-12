26deadacd781460879d72caae3919ff7b192f36b
jevans <john.g.evans.ne@gmail.com>
2015-04-12 18:40:06 -0400

    Run sip.setapi before any other imports
    Otherwise the application will not run on python2
    
    Closes-Issue: #12

--- ctSeg/__init__.py
+++ ctSeg/__init__.py
@@ -1,8 +1,8 @@
 import sip
 
-from . import command_line
-
 # This allows both python2 and python3 to run the same code.
 sip.setapi('QString', 2)
+
+from . import command_line
 
 __all__ = [command_line]
